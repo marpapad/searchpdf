bebfdff5d542c6e0b7742fe03752c5076e17b262
marpapad <marypapadaki13@gmail.com>
2017-08-05 17:34:49 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 00:26:37 2017
+
+@author: DELL PM
+"""
+
+from setuptools import setup
+
+setup(
+     name='searchpdf',
+     version='0.1', 
+     description = 'converts non-searchable pdf files to searchable ones',
+     author = 'marpapad',
+     lisence='UNLISENCE',
+     scripts=['searchpdf.py'] )  
